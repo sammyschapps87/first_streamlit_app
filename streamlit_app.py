4573c9ea5e2182a4347bc5cf44fd391020b405ee
unknown <Samuel.Schappel@sdggroup.local>
2022-12-19 17:07:23 -0500

    creating menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,11 @@
 import streamlit as sl
 
 sl.title('My Parents New Healthy Diner')
-sl.text('Sam is the best')
+sl.header('Breakfast Favorites')
+sl.text('🥣Omega 3 & Blueberry Oatmeal')
+sl.text('🥗Kale, Spinach & Rocket Smoothie')
+sl.text('🐔Hard-Boiled Free-Range Egg')
+sl.text('🥑🍞 Avocado Toast')
+
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
