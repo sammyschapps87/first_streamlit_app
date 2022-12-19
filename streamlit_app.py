8a1ee4a36d7f4471944d6c16a6e10163349154da
unknown <Samuel.Schappel@sdggroup.local>
2022-12-19 17:22:45 -0500

    reactive picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
+fruits_selected = sl.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 sl.title('My Parents New Healthy Diner')
 sl.header('Breakfast Favorites')
@@ -14,5 +15,4 @@ sl.text('🐔Hard-Boiled Free-Range Egg')
 sl.text('🥑🍞 Avocado Toast')
 
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-sl.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
-sl.dataframe(my_fruit_list)
+sl.dataframe(fruits_to_show)
