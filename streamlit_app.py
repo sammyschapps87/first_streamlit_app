b6d43906e318cb3f885b12c791ab7a2e23ba8794
unknown <Samuel.Schappel@sdggroup.local>
2022-12-21 12:35:46 -0500

    finished another page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as sl
 import pandas as pd
-
+import requests
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -18,10 +18,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 sl.dataframe(fruits_to_show)
 
 
-import requests
+
+
 sl.header('Fruityvice Fruit Advice!')
+fruit_choice = sl.text_input('What fruit would you like information about?','Kiwi')
+sl.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-sl.text(fruityvice_response.json())
 
 # normalize json file
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
